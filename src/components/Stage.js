4c9f172dadfc761ba81a429a45e0b9e82e759774
frazermg <mgriffiths29@gmail.com>
2020-05-21 20:35:35 -0700

    useStage section

---
+++ src/components/Stage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyledStage } from './styles/StyledStage';
+
+import Cell from './Cell';
+
+const Stage = ({ stage }) => (
+  <StyledStage width={stage[0].length} height={stage.length}>
+    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+  </StyledStage>
+);
+
+Stage.propTypes = {
+  stage: PropTypes.array,
+};
+
+export default Stage;
